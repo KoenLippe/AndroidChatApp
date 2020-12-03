5035e6056ea656cb96aa11b311b7f96e0afb4a1d
Koen Lippe <koen.lippe@quicknet.nl>
2020-12-03 18:37:48 +0100

    Added test for chat adapter :-)

--- app/src/main/java/com/example/androidchatapp/ui/fragment/chat/ChatFragment.kt
+++ app/src/main/java/com/example/androidchatapp/ui/fragment/chat/ChatFragment.kt
@@ -7,12 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidchatapp.R
+import com.example.androidchatapp.model.ChatListItem
+import com.example.androidchatapp.model.ChatMessage
+import com.example.androidchatapp.ui.ChatAdapter
+import com.example.androidchatapp.ui.adapter.RecentChatsAdapter
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.android.synthetic.main.fragment_chat_overview.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ChatFragment : Fragment() {
+
+    private val chatMessages = arrayListOf<ChatMessage>()
+    private val chatMessagesAdapter = ChatAdapter(chatMessages)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +35,25 @@ class ChatFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        for (x in 0..5) {
+            if(x % 2 == 0) {
+                chatMessages.add(ChatMessage("test", "1", "0101"))
+            } else {
+                chatMessages.add(ChatMessage("test", "2", "0101"))
+            }
+        }
+
+        chatMessagesAdapter.notifyDataSetChanged()
+
+        initRv()
+    }
+
+    private fun initRv() {
+        rvChatLog.apply {
+            adapter = chatMessagesAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL , false)
+        }
     }
 }
